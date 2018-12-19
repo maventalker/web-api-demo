0ff88bfd7499d72ea17c3a14ee69ee18b4bcc137
apple <apple@localhost>
2018-12-19 21:47:15 +0800

    统一异常处理

---
+++ src/main/java/com/growithus/demo/config/MyExceptionHandler.java
@@ -0,0 +1,41 @@
+/**
+ * Project Name:web-api-demo
+ * File Name:ExceptionHandler.java
+ * Package Name:com.growithus.demo.config
+ * Date:2018年12月19日下午9:41:29
+ * Copyright (c) 2018, 杭州知时信息科技有限公司 All Rights Reserved.
+ *
+*/
+
+package com.growithus.demo.config;
+
+import org.apache.log4j.Logger;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.alibaba.fastjson.JSONObject;
+
+/**
+ * ClassName:ExceptionHandler <br/>
+ * Function: <br/>
+ * Date:     2018年12月19日 下午9:41:29 <br/>
+ * @author   apple
+ * @version  
+ * @since    JDK 1.6
+ * @see 	 
+ */
+@RestControllerAdvice
+public class MyExceptionHandler {
+
+    
+    Logger logger = Logger.getLogger(getClass());
+
+    @ExceptionHandler(Exception.class)
+    public String handleException(Exception e) {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("code", 1);
+        jsonObject.put("errMsg", e.getMessage());
+        return jsonObject.toJSONString();
+    }
+}
+
